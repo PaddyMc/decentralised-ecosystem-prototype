34516483173db0ef36c0a0698f01e5a3f64ccbe9
PaddyMc <paddymchale@hotmail.com>
2018-03-23 22:51:18 +0000

    update ca

--- javascript/query.js
+++ javascript/query.js
@@ -40,7 +40,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	fabric_client.setCryptoSuite(crypto_suite);
 
 	// get the enrolled user from persistence, this user will sign all requests
-	return fabric_client.getUserContext('user1', true);
+	return fabric_client.getUserContext('user2', true);
 }).then((user_from_store) => {
 	if (user_from_store && user_from_store.isEnrolled()) {
 		console.log('Successfully loaded user1 from persistence');
