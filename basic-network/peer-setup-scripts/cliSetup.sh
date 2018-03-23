bb3b3a5c774fb1115862f1ff76bf0f7bd088e17d
Cael Rowley <caelrowley@gmail.com>
2018-03-23 23:05:42 +0000

    Created setup scripts to setup the peers and cli and make a query

---
+++ basic-network/peer-setup-scripts/cliSetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ev
+
+peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=peer0.org2.example.com:7051
+CORE_PEER_LOCALMSPID=Org2MSP
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+
+
+CORE_PEER_LOCALMSPID=Org1MSP
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 5
+
+CORE_PEER_LOCALMSPID=Org1MSP
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+
+sleep 2
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=peer0.org2.example.com:7051
+CORE_PEER_LOCALMSPID=Org2MSP
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+peer chaincode query -C mychannel -n fabcar -c '{"Args":["queryAllCars"]}'
+
+
+#
+# peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+# sleep 5
+# peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
---
+++ basic-network/peer-setup-scripts/peer1org2Setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+
+CORE_PEER_ADDRESS=peer1.org2.example.com:7051
+CORE_PEER_LOCALMSPID=Org2MSP
+CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.example.com/msp
+CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/fabric/tls/ca.crt
+
+peer channel join -b mychannel.block
+
+peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+# get mychannel.block
